fb1ce8ff31cc02e0767d85ecdc90181d74bf1a7f
Katta Omkar <omkar@LAPTOP-OIM65QIN.localdomain>
2022-11-21 17:00:24 +0530

    Hiding Variables

--- utils/dbconfig.py
+++ utils/dbconfig.py
@@ -2,15 +2,21 @@ import mysql.connector
 from rich import print as printc
 from rich.console import Console
 console = Console()
+
+# for Loading env variables
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
   
 def dbconfig():
   try:
     db = mysql.connector.connect(
-      host ="pmmariadb.mariadb.database.azure.com",
-      user ="pmadmin@pmmariadb",
-      passwd ="xA7@HBbEpQ4XE@R5"
+      host =os.getenv("HOST"),
+      user =os.getenv("USER"),
+      passwd =os.getenv("PASSWD")
     )
-    # printc("[green][+][/green] Connected to db")
+    # printc("[green][+][/green] Connected to db")  
   except Exception as e:
     print("[red][!] An error occurred while trying to connect to the database[/red]")
     console.print_exception(show_locals=True)
